29b73730f053d86a6a1b2a8a0b39fce89ecded49
bavuchoko <bavuchoko@naver.com>
2025-11-05 00:37:04 +0900

    Initial Commit

---
+++ LunarCal/LunarCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunarCalApp.swift
+//  LunarCal
+//
+//  Created by 박종수 on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct LunarCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
